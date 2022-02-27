370bc8e221886db20f5b408990f92b49980f97da
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-02-27 13:31:59 +0000

    Code Clean Up

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import csv
-import sdl2
 from engine import *
 
-world = Engine()
+#  ONLY VARIABLES THAT NEED CHANGING  #
+#######################################
+
+ROLE = "tank"
+JOB = "pld"
+ENCOUNTER = "p2s"
+
+PERFECT_PARSE = f"{SLN_PATH}/perfect/{ROLE}/{JOB}/{ENCOUNTER}.txt"
+XIV_PARSE = f"{XIV_PATH}/jobs/{ROLE}/{JOB}/parsed/{ENCOUNTER}.txt"
+
+PARSE = XIV_PARSE
+
+#########################################
+
+window = Engine()
 actions = []
 gcd_actions = []
 ogcd_actions = []
@@ -17,52 +30,51 @@ mage_gcd = 2.50
 mage_gcd_gap = move_speed * mage_gcd
 
 activation_time = 100
-# 0 is about ~ 3seconds ish
 # 8 seconds - Use on 10s pre pull
 countdown = (60 * 5) - 30
 
-rel_fight_path = f"{SLN_PATH}/fight.txt"
-rel_encounter_path = f"{SLN_PATH}/encounters.json"
-rel_jobs_path = f"{SLN_PATH}/game/roles/"
+fight_path = f"{SLN_PATH}/fight.txt"
+encounter_path = f"{SLN_PATH}/encounters.json"
+jobs_path = f"{SLN_PATH}/game/roles/"
 
 matchesMelee = []
 matchesMage = []
 matchesOgcd = []
+
 halfSecondSpacer = "Half-Second"
 halfTenthSpacer = "Half-Tenth"
 
-
-pictures = {}
+ability_icons = {}
 
 def load_images(role, job):
-    factory = world.factory
-    pictures["Activation"] = factory.from_image(f"{SLN_PATH}/resources/Activation.png")
+    factory = window.factory
+    ability_icons["Activation"] = factory.from_image(f"{SLN_PATH}/resources/Activation.png")
 
-    with open(f"{rel_jobs_path}/{role}/icon_map.csv") as tank_file:
-        next(tank_file)
-        csv_reader = csv.reader(tank_file, delimiter=',')
+    with open(f"{jobs_path}/{role}/icon_map.csv") as role_file:
+        next(role_file)
+        csv_reader = csv.reader(role_file, delimiter=',')
         for row in csv_reader:
-                pictures[row[0]] = factory.from_image(f"{SLN_PATH}/resources/{role}/{row[1]}")
+                ability_icons[row[0]] = factory.from_image(f"{SLN_PATH}/resources/{role}/{row[1]}")
     
-    with open(f"{rel_jobs_path}/{role}/{job}/icon_map.csv") as job_file:
+    with open(f"{jobs_path}/{role}/{job}/icon_map.csv") as job_file:
         next(job_file)
         csv_reader = csv.reader(job_file, delimiter=',')
         for row in csv_reader:
-            pictures[row[0]] = factory.from_image(f"{SLN_PATH}/resources/{role}/{job}/{row[1]}")
+            ability_icons[row[0]] = factory.from_image(f"{SLN_PATH}/resources/{role}/{job}/{row[1]}")
 
 def load_actions(role, job):
     isGCD = False
     isMelee = False
 
-    with open(f"{rel_jobs_path}/{role}/actions.txt", 'r') as file1:
-        for line in file1:
+    with open(f"{jobs_path}/{role}/actions.txt", 'r') as role_file:
+        for line in role_file:
             line = line.rstrip()
             if line == "oGCD":
                 continue
             matchesOgcd.append(line)
 
-    with open(f"{rel_jobs_path}/{role}/{job}/actions.txt", 'r') as file:
-        for line in file:
+    with open(f"{jobs_path}/{role}/{job}/actions.txt", 'r') as job_file:
+        for line in job_file:
             line = line.rstrip()
             if line == "GCD":
                 isGCD = True
@@ -83,57 +95,12 @@ def load_actions(role, job):
             if isGCD == False:
                 matchesOgcd.append(line)
 
-def load_encounter_xivanalysis(wrld, role, job, file):
+def load_encounter(wrld, file):
     global actions
-    foreground.append(Entity(wrld, pictures["Activation"], activation_time, 0, 5, 50, (100,0,10,255)))
-   
-    with open(f"{XIV_PATH}/jobs/{role}/{job}/parsed/{file}.txt") as file:
-        for line in file:
-            actions.append(line.rstrip())
-            
-    index = 0
-    gcd_gap_index = 1
-    timeline = 1
-    while index < len(actions):
-        if any(x in actions[index] for x in matchesMelee):
-            timeline += 2.45
-            gcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
-            index += 1
-
-        if index < len(actions):
-            if any(x in actions[index] for x in matchesMage):
-                timeline += 2.50
-                gcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
-                index += 1
-
-        if index < len(actions):
-            if any(x in actions[index] for x in matchesOgcd):
-                ogcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 50, 5, 25, 25))
-                index += 1
-
-        if index < len(actions):
-            if any(x in actions[index] for x in matchesOgcd):
-                ogcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 85, 5, 25, 25))
-                index += 1 
-
-        if index < len(actions):
-            if actions[index] == halfSecondSpacer:
-                timeline += 0.5
-                index += 1      
-
-        if index < len(actions):
-            if actions[index] == halfTenthSpacer:
-                timeline += 0.05
-                index += 1       
-
-        gcd_gap_index += 1
-
-def load_encounter_personal(wrld, role, job, file):
-    global actions
-    foreground.append(Entity(wrld, pictures["Activation"], activation_time, 0, 5, 50, (100,0,10,255)))
+    foreground.append(Entity(wrld, ability_icons["Activation"], activation_time, 0, 5, 50, (100,0,10,255)))
     
-    with open(f"{SLN_PATH}/perfect/{role}/{job}/{file}.txt") as file:
-        for line in file:
+    with open(file) as encounter_file:
+        for line in encounter_file:
             actions.append(line.rstrip())
 
     index = 0
@@ -142,23 +109,23 @@ def load_encounter_personal(wrld, role, job, file):
     while index < len(actions):
         if any(x in actions[index] for x in matchesMelee):
             timeline += 2.45
-            gcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
+            gcd_actions.append(Entity(wrld, ability_icons[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
             index += 1
 
         if index < len(actions):
             if any(x in actions[index] for x in matchesMage):
                 timeline += 2.50
-                gcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
+                gcd_actions.append(Entity(wrld, ability_icons[actions[index]], (activation_time + countdown) + (timeline * move_speed), 5))
                 index += 1
 
         if index < len(actions):
             if any(x in actions[index] for x in matchesOgcd):
-                ogcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 50, 5, 25, 25))
+                ogcd_actions.append(Entity(wrld, ability_icons[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 50, 5, 25, 25))
                 index += 1
 
         if index < len(actions):
             if any(x in actions[index] for x in matchesOgcd):
-                ogcd_actions.append(Entity(wrld, pictures[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 85, 5, 25, 25))
+                ogcd_actions.append(Entity(wrld, ability_icons[actions[index]], (activation_time + countdown) + (timeline * move_speed) + 85, 5, 25, 25))
                 index += 1 
 
         if index < len(actions):
@@ -173,27 +140,26 @@ def load_encounter_personal(wrld, role, job, file):
 
         gcd_gap_index += 1
 
-@world.draw
+@window.draw
 def draw():
     [entity.draw() for entity in background]
     [entity.draw() for entity in gcd_actions]
     [entity.draw() for entity in ogcd_actions]
     [entity.draw() for entity in foreground]
 
-@world.update
+@window.update
 def update(dt):
     [entity.update(move_speed * dt) for entity in gcd_actions]
     [entity.update(move_speed * dt) for entity in ogcd_actions]
     if gcd_actions:
         if gcd_actions[0].rect.x < -50:
             gcd_actions.pop(0)
+    if ogcd_actions:
+        if ogcd_actions[0].rect.x < -50:
+            ogcd_actions.pop(0)
 
-role = "tank"
-job = "pld"
+load_images(ROLE, JOB)
+load_actions(ROLE, JOB)
+load_encounter(window, PARSE)
 
-load_images(role, job)
-load_actions(role, job)
-#load_encounter_xivanalysis(world, role, job, "p1s")
-load_encounter_personal(world, role, job, "p2s")
-
-world.loop()
+window.loop()
--- xivanalysis/fetch-parse.py
+++ xivanalysis/fetch-parse.py
@@ -2,33 +2,32 @@ import json
 
 role = "tank"
 job = "drk"
-# source ID - have this in another file to stop recompile?
-# data.json - open xivanalysis open network inspector refresh open largest file in new tab, copy to data.json file
-XIV_PATH = "C:/Users/Ryan/Documents/Git/rotation-helper/xivanalysis"
-rel_config_path = f"{XIV_PATH}/config.txt"
 actionsToIgnore = ["attack", "Iron Will", "Provoke", "Hallowed Ground", "Intervention"]
+actions = []
+
+XIV_PATH = "C:/Users/Ryan/Documents/Git/rotation-helper/xivanalysis"
+config_path = f"{XIV_PATH}/config.txt"
 
 #   1. Source ID
 #   2. Input File
+#       - data.json - open xivanalysis open network inspector refresh open largest file in new tab, copy to data.json file
 config = []
-with open(rel_config_path) as file:
+with open(config_path) as file:
     for line in file:
         text = line.rstrip('\n')
         config.append(text)
 
 sourceID = int(config[0])
-rel_input_path = f"{XIV_PATH}/jobs/{role}/{job}/json/{config[1]}.json"
-rel_output_path = f"{XIV_PATH}/jobs/{role}/{job}/parsed/{config[1]}.txt"
+input_path = f"{XIV_PATH}/jobs/{role}/{job}/json/{config[1]}.json"
+output_path = f"{XIV_PATH}/jobs/{role}/{job}/parsed/{config[1]}.txt"
 
-actions = []
-with open(rel_input_path) as json_file:
+with open(input_path) as json_file:
     encounter = json.load(json_file)
     for entry in encounter['events']:
         try:
             entry["sourceID"] == sourceID
         except:
             continue
-        
         if (
             entry["type"] == "cast" and
             entry["sourceID"] == sourceID and
@@ -36,5 +35,5 @@ with open(rel_input_path) as json_file:
         ):    
             actions.append(entry["ability"]["name"])
 
-with open(rel_output_path, 'w') as f:
+with open(output_path, 'w') as f:
     f.write('\n'.join(actions))
