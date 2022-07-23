786910e4b05a13c48980f81dc81711ba692a426b
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-07-23 16:59:08 +0100

    made fetch parse more dynamic figuring out player id

--- xivanalysis/fetch-parse.py
+++ xivanalysis/fetch-parse.py
@@ -12,28 +12,27 @@ class Flag():
 #  ONLY VARIABLES THAT NEED CHANGING in Config.txt  #
 #####################################################
 XIV_PATH = f"{os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))}/xivanalysis"
-CONFIG_PATH = f"{XIV_PATH}/config.txt"
 IGNORE_ACTION = ["attack", "Iron Will", "Provoke", "Hallowed Ground", "Intervention", "Interject", "Shield Wall", "Stronghold", "Last Bastion", "Land Waker", "Dark Force", "Gunmetal Soul"]
 #####################################################
 
 actions = []
 config = []
 
-def main():
+identifiers = {
+  "pld": "Intervene"
+}
 
-    with open(CONFIG_PATH) as file:
-        for line in file:
-            text = line.split(":",1)[1]
-            text = text.strip()
-            config.append(text)
+ROLE = "tank"
+JOB  = "pld"
 
-    sourceID = int(config[Flag.SOURCEID])
-    action_output_path = f"{XIV_PATH}/jobs/{config[Flag.ROLE]}/{config[Flag.JOB]}/{config[Flag.ENCOUNTER]}.txt"
-    
+def single_file(fight_name, fight_url, override = False):
+
+    if os.path.exists(f"{XIV_PATH}/jobs/{ROLE}/{JOB}/{fight_name}.txt") and not override:
+        print(f"{fight_name} file already exists!")
+        return
+
     conn = http.client.HTTPSConnection("xivanalysis.com")
-
     payload = ""
-
     headers = {
         'authority': "xivanalysis.com",
         'accept': "application/json",
@@ -47,14 +46,21 @@ def main():
         'sec-fetch-site': "same-origin",
         'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
         }
-
-    conn.request("GET", f"/proxy/fflogs/report/events/{config[Flag.URLREQUEST]}", payload, headers)
+    conn.request("GET", f"/proxy/fflogs/report/events/{fight_url}", payload, headers)
 
     result = conn.getresponse()
     data = result.read()
     string = data.decode("utf-8")
 
     encounter = json.loads(string)
+    for entry in encounter['events']:
+        try:
+            if entry['ability']['name'] == identifiers[JOB]:
+                sourceID = entry["sourceID"]
+                break
+        except:
+            continue
+
     for entry in encounter['events']:
         try:
             entry["sourceID"] == sourceID
@@ -64,13 +70,25 @@ def main():
             entry["type"] == "cast" and
             entry["sourceID"] == sourceID and
             not any(x in entry["ability"]["name"] for x in IGNORE_ACTION)
-        ):    
+        ):
             actions.append(entry["ability"]["name"])
             actions.append(entry["timestamp"])
 
+    action_output_path = f"{XIV_PATH}/jobs/{ROLE}/{JOB}/{fight_name}.txt"
     with open(action_output_path, 'w') as f:
         for line in actions:
             f.write(f"{line}\n")
+    actions.clear()
+
+def raid_tier():
+    single_file("p1s", "N6nvcKYmQC2d3M4W?start=1994517&end=2406988&translate=true")
+    single_file("p2s", "W6Avqfd1T9mx3FGg?start=450362&end=991804&translate=true")
+    single_file("p3s", "wZKRXbpP7knF1TQY?start=971086&end=1516533&translate=true")
+    single_file("p4sp1", "JfG2yaQPLHYgqR8c?start=216064&end=606434&translate=true")
+
+def main():
+    #single_file("p1s", "N6nvcKYmQC2d3M4W?start=1994517&end=2406988&translate=true")
+    raid_tier()
 
 if __name__ == '__main__':
     main()
