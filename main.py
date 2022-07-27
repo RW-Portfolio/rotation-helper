9b33958553a606004776f9337eabaa8563973dbe
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-07-27 20:24:35 +0100

    packaged project

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from engine import *
+import engine.engine as ENGINE
+import engine.constants as CONSTANTS
+import engine.entity as ENTITY
+## TODO Implement this
+import xivanalysis.fetch_parse as XIV
 
 #  ONLY VARIABLES THAT NEED CHANGING  #
 #######################################
@@ -6,12 +10,12 @@ ROLE = "tank"
 JOB = "drk"
 ENCOUNTER = "p3s"
 
-XIV_PARSE = f"{XIV_PATH}/jobs/{ROLE}/{JOB}/{ENCOUNTER}.txt"
+XIV_PARSE = f"{CONSTANTS.XIV_PATH}/jobs/{ROLE}/{JOB}/{ENCOUNTER}.txt"
 
 PARSE = XIV_PARSE
 #########################################
 
-window = Engine()
+window = ENGINE.Engine()
 actions = []
 gcd_actions = []
 ogcd_actions = []
@@ -27,27 +31,27 @@ timings = []
 types = ["GCD", "oGCD", "Melee", "Mage"]
 
 def load_images_from_txt(file_loc):
-    with open(f"{JOBS_PATH}/{file_loc}/actions.txt") as role_file:
+    with open(f"{CONSTANTS.JOBS_PATH}/{file_loc}/actions.txt") as role_file:
         for line in role_file:
             if any(x in line for x in types):
                 continue
             image_name = line.replace(" ", "_").strip('\n')
             image_name += ".png"           
-            ability_icons[line.strip('\n')] = window.factory.from_image(f"{SLN_PATH}/resources/{file_loc}/{image_name}")
+            ability_icons[line.strip('\n')] = window.factory.from_image(f"{CONSTANTS.SLN_PATH }/resources/{file_loc}/{image_name}")
 
 def load_images(role, job):
-    ability_icons["Activation"] = window.factory.from_image(f"{SLN_PATH}/resources/Activation.png")
+    ability_icons["Activation"] = window.factory.from_image(f"{CONSTANTS.SLN_PATH}/resources/Activation.png")
     load_images_from_txt(f"{role}")
     load_images_from_txt(f"{role}/{job}")
 
 def load_actions(role, job):
-    with open(f"{JOBS_PATH}/{role}/actions.txt", 'r') as role_file:
+    with open(f"{CONSTANTS.JOBS_PATH}/{role}/actions.txt", 'r') as role_file:
         for line in role_file:            
             if line.rstrip() != "oGCD":
                 is_ogcd.append(line.rstrip())  
         
     isMelee, isGCD = False, False
-    with open(f"{JOBS_PATH}/{role}/{job}/actions.txt", 'r') as job_file:
+    with open(f"{CONSTANTS.JOBS_PATH}/{role}/{job}/actions.txt", 'r') as job_file:
         for line in job_file:
             line = line.rstrip()
             if line == "GCD":   isGCD = True
@@ -76,7 +80,7 @@ def add_timings_ogcd(index):
     return index
 
 def load_timings(file):
-    with open(f"{XIV_PATH}/jobs/{ROLE}/{JOB}/{ENCOUNTER}.txt") as timing_file:
+    with open(f"{CONSTANTS.XIV_PATH}/jobs/{ROLE}/{JOB}/{ENCOUNTER}.txt") as timing_file:
         for line in timing_file:
             actions.append(line.rstrip())
 
@@ -94,7 +98,7 @@ def add_encounter_gcd(index, timeline, cast_type):
     if index < len(actions):
             if any(x in actions[index] for x in cast_type):
                 timeline += float(timings.pop(0))
-                gcd_actions.append(Entity(window, ability_icons[actions[index]], (ACTIVATION_TIME + COUNDOWN) + (timeline * MOVE_SPEED), 5))
+                gcd_actions.append(ENTITY.Entity(window, ability_icons[actions[index]], (CONSTANTS.ACTIVATION_TIME + CONSTANTS.COUNDOWN) + (timeline * CONSTANTS.MOVE_SPEED), 5))
                 index += 1 
                 return index, timeline
     return index, timeline
@@ -102,13 +106,13 @@ def add_encounter_gcd(index, timeline, cast_type):
 def add_encounter_ogcd(index, timeline, position):
     if index < len(actions):
         if any(x in actions[index] for x in is_ogcd):
-            ogcd_actions.append(Entity(window, ability_icons[actions[index]], (ACTIVATION_TIME + COUNDOWN) + (timeline * MOVE_SPEED) + position, 5, 25, 25))
+            ogcd_actions.append(ENTITY.Entity(window, ability_icons[actions[index]], (CONSTANTS.ACTIVATION_TIME + CONSTANTS.COUNDOWN) + (timeline * CONSTANTS.MOVE_SPEED) + position, 5, 25, 25))
             index += 1
             return index
     return index
 
 def load_encounter(window):
-    foreground.append(Entity(window, ability_icons["Activation"], ACTIVATION_TIME, 0, 5, 50, (100,0,10,255)))
+    foreground.append(ENTITY.Entity(window, ability_icons["Activation"], CONSTANTS.ACTIVATION_TIME, 0, 5, 50, (100,0,10,255)))
     del actions[1::2]   
     
     index, timeline = 0, 1
@@ -131,8 +135,8 @@ def draw():
 
 @window.update
 def update(dt):
-    [entity.update(MOVE_SPEED * dt) for entity in gcd_actions]
-    [entity.update(MOVE_SPEED * dt) for entity in ogcd_actions]
+    [entity.update(CONSTANTS.MOVE_SPEED * dt) for entity in gcd_actions]
+    [entity.update(CONSTANTS.MOVE_SPEED * dt) for entity in ogcd_actions]
     remove_out_of_bounds_obj(gcd_actions)
     remove_out_of_bounds_obj(ogcd_actions)
 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages  
+setup(
+    name='rotation-helper',
+    version='0.1.0',
+    description='XIV Helper',
+    author='Ryan Westwood',
+    author_email='ryanwestwood7@outlook.com',
+    packages=find_packages()
+)
+
